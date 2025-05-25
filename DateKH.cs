89a30a3a6d32bd7221042c759a30bc427cf3e73d
chril sim <simchril@gmail.com>
2025-05-25 21:50:53 +0700

    Add files via upload

---
+++ DateKH.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KHDate
+{
+    
+    public class DateKH
+    {
+       
+        public  static string GetKhmerDate(DateTime CreateAt)
+        {
+            string[] khmerMonths = {
+            "មករា", "កម្ភៈ", "មីនា", "មេសា", "ឧសភា", "មិថុនា",
+            "កក្កដា", "សីហា", "កញ្ញា", "តុលា", "វិច្ឆិកា", "ធ្នូ"
+            };
+
+            return $"{CreateAt.Day:00} {khmerMonths[CreateAt.Month - 1]} {CreateAt.Year}";
+        }
+
+        public static string UpdateGetKhmerDate(DateTime UpdateAt)
+        {
+            DateTime date = DateTime.Now;
+            string[] khmerMonths = {
+                    "មករា", "កម្ភៈ", "មីនា", "មេសា", "ឧសភា", "មិថុនា",
+                    "កក្កដា", "សីហា", "កញ្ញា", "តុលា", "វិច្ឆិកា", "ធ្នូ"
+                    };
+             return $"{date.Day:00} {khmerMonths[date.Month - 1]} {date.Year} ម៉ោង {date.Hour:00}:{date.Minute:00}";
+        }
+    }
+}
